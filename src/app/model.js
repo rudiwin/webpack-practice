57f23690860d12cf793c8331c0cdd8cf65c5a2c3
Rudi Winanda <ruwindev@gmail.com>
2023-10-02 16:31:41 +0700

    refactor: add utils and config files

--- src/app/model.js
+++ src/app/model.js
@@ -1,14 +1,13 @@
+import { getJSON } from './utils/helpers';
+import { API_URL } from './config';
+
 const state = {
   recipe: {},
 };
 
 const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
---
+++ src/app/utils/helpers.js
@@ -0,0 +1,18 @@
+import { TIMEOUT_SEC } from '../config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(() => {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+const getJSON = async function (url) {
+  const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
+  const data = await res.json();
+  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+  return data;
+};
+
+export { getJSON };
